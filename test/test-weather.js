d95cff609f2fa28e95e5102f2cd58e88fe76f85b
Joshua Hanson <nibblebot@gmail.com>
2020-05-17 02:19:05 -0500

    feature/test: fetch/display weather for zip code

--- test/test-weather.js
+++ test/test-weather.js
@@ -1,6 +1,6 @@
 const expect = require("chai").expect
 const nock = require("nock")
-const { fetchWeatherForCity } = require("../weather")
+const { fetchWeather } = require("../weather")
 const responses = require("./responses")
 
 describe("weather", () => {
@@ -11,9 +11,21 @@ describe("weather", () => {
       )
       .reply(200, responses[0])
 
-    const weatherForCity = await fetchWeatherForCity("Marrero")
+    const weatherForCity = await fetchWeather("Marrero")
     expect(weatherForCity).to.equal(
       "Marrero, US: Clouds - Temp 24°C - Humidity 83% - Wind 3.1kph - Time - 9:28 PM GMT-5"
+    )
+  })
+  it("should fetch and parse weather for zipcode", async () => {
+    nock("https://api.openweathermap.org")
+      .get(
+        "/data/2.5/weather?zip=70072&units=metric&appid=4fe760a65625e12e8cf362d264c244de"
+      )
+      .reply(200, responses[1])
+
+    const weatherForCity = await fetchWeather("70072")
+    expect(weatherForCity).to.equal(
+      "Marrero, US: Clouds - Temp 24°C - Humidity 94% - Wind 4.1kph - Time - 2:04 AM GMT-5"
     )
   })
 })
--- weather.ts
+++ weather.ts
@@ -8,7 +8,8 @@ if (!argv.length) {
   console.error(`usage: ./weather.ts <city>`)
   process.exit(1)
 }
-const city = argv.join(" ")
+const args = argv.join(" ")
+
 const API_KEY = "4fe760a65625e12e8cf362d264c244de"
 const getJSON = bent("json")
 
@@ -40,32 +41,45 @@ function formatTime(unixTimestamp, tzOffsetSeconds) {
   return format(adjustedDate, "h:mm aa ") + GMTOffset
 }
 
-export const fetchWeatherForCity = async (city: string) => {
-  const params = {
-    q: city,
+function formatWeatherAndTime(json) {
+  const weatherDescription = json.weather[0].main
+  const temp = Math.floor(json.main.temp) + "°C"
+  const humidity = json.main.humidity + "%"
+  const wind = json.wind.speed + "kph"
+  const location = `${json.name}, ${json.sys.country}`
+  const formattedTime = formatTime(json.dt, json.timezone)
+  return `${location}: ${weatherDescription} - Temp ${temp} - Humidity ${humidity} - Wind ${wind} - Time - ${formattedTime}`
+}
+
+export const fetchWeather = async (query: string) => {
+  const params: {
+    units: string
+    appid: string
+    q?: string
+    zip?: string
+  } = {
     units: "metric",
     appid: API_KEY,
   }
-  const apiQuerystring = querystring.stringify(params)
-  const weatherURL =
-    "https://api.openweathermap.org/data/2.5/weather?" + apiQuerystring
+  if (/\d{5}/.test(query)) {
+    params.zip = query
+  } else {
+    params.q = query
+  }
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?" +
+    querystring.stringify(params)
   try {
-    const json: APIResponse = await getJSON(weatherURL)
-    const weatherDescription = json.weather[0].main
-    const temp = Math.floor(json.main.temp) + "°C"
-    const humidity = json.main.humidity + "%"
-    const wind = json.wind.speed + "kph"
-    const location = `${json.name}, ${json.sys.country}`
-    const formattedTime = formatTime(json.dt, json.timezone)
-    return `${location}: ${weatherDescription} - Temp ${temp} - Humidity ${humidity} - Wind ${wind} - Time - ${formattedTime}`
+    const json: APIResponse = await getJSON(url)
+    return formatWeatherAndTime(json)
   } catch (err) {
     console.error(err)
-    return `${city}: Invalid City`
+    return `${query}: Invalid City or Zip Code`
   }
 }
 
 if (require.main === module) {
   ;(async () => {
-    console.log(await fetchWeatherForCity(city))
+    console.log(await fetchWeather(args))
   })()
 }
